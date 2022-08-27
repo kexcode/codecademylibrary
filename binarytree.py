41b7f4231fe30bc66319aa5d8c0e6b16760ff130
kexcoder <33663607+kexcode@users.noreply.github.com>
2022-08-27 14:44:23 +0200

    Create binarytree.py
    Binary tree search added

---
+++ binarytree.py
@@ -0,0 +1,31 @@
+class BinarySearchTree:
+  def __init__(self, value, depth=1):
+    self.value = value
+    self.depth = depth
+    self.left = None
+    self.right = None
+
+  def insert(self, value):
+    if (value < self.value):
+      if (self.left is None):
+        self.left = BinarySearchTree(value, self.depth + 1)
+        print(f'Tree node {value} added to the left of {self.value} at depth {self.depth + 1}')
+      else:
+        self.left.insert(value)
+    else:
+      if (self.right is None):
+        self.right = BinarySearchTree(value, self.depth + 1)
+        print(f'Tree node {value} added to the right of {self.value} at depth {self.depth + 1}')
+      else:
+        self.right.insert(value)
+        
+  # Define .get_node_by_value() below:
+  def get_node_by_value(self, value):
+    if self.value == value:
+      return self
+    elif self.left and value < self.value:
+      return self.left.get_node_by_value(value)
+    elif self.right and value > self.value:
+      return self.right.get_node_by_value(value)
+    else:
+      return None
